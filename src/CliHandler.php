22b7110ebe55d3bf8be136d30264c17ade4b3585
k-yamamoto-balocco <k.yamamoto@balocco.info>
2023-03-29 14:11:38 +0900

    ハンドラクラスにパラメタ用のプロパティを追加

--- src/CliHandler.php
+++ src/CliHandler.php
@@ -3,8 +3,11 @@
 namespace GitBalocco\LaravelUiCli;
 
 use GitBalocco\LaravelUiCli\Contract\CliHandlerInterface;
+use GitBalocco\LaravelUiCli\Contract\CliParameterInterface;
 
 abstract class CliHandler implements CliHandlerInterface
 {
-
+    public function __construct(private CliParameterInterface $parameter)
+    {
+    }
 }
